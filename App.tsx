2547097ca6c57800f2aa41d6d1f9d17771bde4b3
Ru Song <phuahrusong@live.com>
2025-03-21 13:08:04 +0800

    feat: add App.tsx with AuthContext, AppNavigator and ToastConfig

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//third party libraries
+import Toast from 'react-native-toast-message';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//commponents
+import { toastConfig } from './src/config/toastConfig';
+import { AuthProvider } from './src/context/AuthContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+const App: React.FC = () => {
+    return (
+        <AuthProvider>
+            <NavigationContainer>
+                <AppNavigator />
+                <Toast config={toastConfig} />
+            </NavigationContainer>
+        </AuthProvider>
+    );
+};
+
+export default App;
+
