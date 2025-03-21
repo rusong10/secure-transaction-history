561bebb095e46c0571b865989a8f27105352872c
Ru Song <phuahrusong@live.com>
2025-03-21 13:10:10 +0800

    feat: add custom colors configuration

---
+++ src/config/colors.tsx
@@ -0,0 +1,8 @@
+export default Object.freeze({
+    blueMain: '#0A00DF',
+    blueSecond: '#007bff',
+    buttonBlue: '#6AA3F9',
+    header: '#5c5c5c',
+    body: '#757575',
+    lineGrey: '#ededed',
+})
